a1a4823b6392d724da7965fef7c056ad124ffe4f
M1D1y <admiral.kanaris4@gmail.com>
2025-09-18 20:15:35 +0300

    LR_1

--- src/Primes.java
+++ src/Primes.java
@@ -9,7 +9,6 @@ public class Primes {
     }
 
     public static void main(String[] args) {
-        // Вывод простых чисел от 1 до 100
         for (int i = 1; i <= 100; i++) {
             if (isPrime(i)) {
                 System.out.print(i + " ");
